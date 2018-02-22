brandonscript/fylm

commit 9a33aa97d33cf7fd8b3c9768d71baffc1c44c14e

Support TMDb search for poorly named files/folders in interactive mode

fylm/__init__.py
@@ -74,27 +74,27 @@ def main():
             for film in films:
 
                 # If the film should be ignored, print the reason why, and skip.
-                if film.should_ignore is True:
-                    console.skip(film)
-                    continue
-
-                # Print the film details to the console.
-                console.film_loaded(film)
-
-                # Search TMDb for film details (if enabled).
-                film.search_tmdb()
-
-                # If the search failed, or TMDb is disabled, print why, and skip.
-                if film.tmdb_id is None and config.tmdb.enabled is True and config.interactive is False:
+                if film.should_ignore is True and config.interactive is False:
                     console.skip(film, film.ignore_reason)
                     continue
 
-                # If the lookup was successful, print the results to the console.
-                console.search_result(film)
+                # Print film header to console.
+                console.film_loaded(film)
 
+                # Search TMDb for film details (if enabled).
+                search = film.search_tmdb()
+
+                if search is False and config.interactive is False:
+                    console.skip(film, film.ignore_reason)
+                    continue
+
+                # Print the film details to the console.
                 if config.interactive is True:
-                    if not interactive.verify_film(film):
+                    if interactive.start(film) is False:
                         continue
+                else:
+                    # If the lookup was successful, print the results to the console.
+                    console.search_result(film)
 
                 # If duplicate checking is enabled and the film is a duplicate, abort,
                 # *unless* overwriting is enabled. `is_duplicate` will always return

fylm/fylmlib/film.py
@@ -313,22 +313,28 @@ class Film:
         Calls the tmdb.search() method, passing the current film
         as params. If a result is found, the current film's properties
         are updated with the values from TMDb.
+
+        Returns:
+            True if a suitable match was found, else False
         """
 
         # Only perform lookups if TMDb searching is enabled.
-        if config.tmdb.enabled is True:
+        if config.tmdb.enabled is False:
+            return False
 
-            # Perform the search and save the first 10 results to the matches list. 
-            # If ID is not None, search by ID.
-            self.matches = (tmdb.search(self.tmdb_id) if (self.tmdb_id is not None) else tmdb.search(self.title, self.year))[:10]
-            best_match = next(iter(self.matches or []), None)
-            
-            if best_match is not None:
-                # If we find a result, update title, tmdb_id, year, and the title_similarity.
-                self.update_with_match(best_match)
-            else:
-                # If not, we update the ignore_reason
-                self.ignore_reason = 'No results found'
+        # Perform the search and save the first 10 results to the matches list. 
+        # If ID is not None, search by ID.
+        self.matches = (tmdb.search(self.tmdb_id) if (self.tmdb_id is not None) else tmdb.search(self.title, self.year))[:10]
+        best_match = next(iter(self.matches or []), None)
+        
+        if best_match is not None:
+            # If we find a result, update title, tmdb_id, year, and the title_similarity.
+            self.update_with_match(best_match)
+            return True
+        else:
+            # If not, we update the ignore_reason
+            self.ignore_reason = 'No results found'
+            return False
 
 
     def update_with_match(self, match):

fylm/fylmlib/interactive.py
@@ -27,6 +27,7 @@ import readline
 
 from colors import color, bold
 
+from fylmlib.config import config
 from fylmlib.parser import parser
 from fylmlib.console import console
 from fylmlib.ansi import ansi
@@ -35,6 +36,26 @@ INPUT_PROMPT = '»'
 MESSAGE_PREFIX = '      '
 
 class interactive:
+
+    @classmethod
+    def start(cls, film):
+        """Main router for handling a known or unknown film.
+
+        Determines whether the user should be prompted to verify a
+        matching film, or look up an unknown one.
+        
+        Args:
+            film: (Film) Current film to process
+        Returns:
+            True if the film passes verification or interactive mode is 
+            disabled, else False
+        """
+        if config.interactive is False:
+            return True
+        elif film.should_ignore and film.ignore_reason.startswith('Unknown'):
+            return cls.handle_unknown_film(film)
+        else:
+            return cls.verify_film(film)
 
     @classmethod
     def verify_film(cls, film):
@@ -49,6 +70,7 @@ class interactive:
         Returns:
             True if the film passes verification, else False
         """
+        console.search_result(film)
         console.ask('Is this correct? [Y]')
         choice = cls._choice_input(
             prompt='', 
@@ -69,7 +91,38 @@ class interactive:
             console.dim("Skipped")
             return False
         else:
-            return film.tmdb_verified              
+            return film.tmdb_verified  
+
+    @classmethod
+    def handle_unknown_film(cls, film):
+        """Ask the user whether an unknown film should be manually 
+        searched for or skipped.
+        
+        Args:
+            film: (Film) Current film to process
+        Returns:
+            True if the film should be processed, else False
+        """ 
+        console.ask("Unknown title or year [N]")
+
+        # Continuously loop this if an invalid choice is entered.
+        while True:
+            choice = cls._choice_input(
+                prompt='', 
+                choices=[
+                    ('N', 'Search by name'),
+                    ('I', 'Lookup by ID'),
+                    ('S', '[ Skip ]')],
+                default='N')
+
+            if choice == 0:
+                return cls.search_by_name(film)
+            elif choice == 1:
+                return cls.lookup_by_id(film)
+            elif choice == 2:
+                film.ignore_reason = 'Skipped'
+                console.dim("Skipped")
+                return False           
 
     @classmethod
     def lookup_by_id(cls, film):
@@ -83,18 +136,26 @@ class interactive:
             True if the film passes verification, else False
         """
         while True:
+
+            # Delete the existing ID in case it is a mismatch.
             film.tmdb_id = None
             search = cls._simple_input('TMDb ID ')
             try:
+                # Attempt to convert the search query to an int and update
+                # the film.
                 film.tmdb_id = int(search)
                 try:
+                    # Search for the new film by ID.
                     film.search_tmdb()
-                    console.search_result(film)
+
+                    # Verify the search result.
                     return cls.verify_film(film)
-                except Exception:
+                except Exception as e:
                     console.colored("%sHrm, that ID doesn't exist" % MESSAGE_PREFIX, ansi.pink)
-            except Exception:
+                    console.debug(e)
+            except Exception as e:
                 console.colored("%sA TMDb ID must be a number" % MESSAGE_PREFIX, ansi.pink) 
+                console.debug(e)
 
     @classmethod
     def search_by_name(cls, film):
@@ -109,7 +170,7 @@ class interactive:
             True or False, passing the return value from choose_from_matches
         """
         film.tmdb_id = None
-        search = cls._simple_input('Search TMDb ', '%s %s' % (film.title, film.year))
+        search = cls._simple_input('Search TMDb ', '%s %s' % (film.title, film.year or ''))
         film.title = parser.get_title(search)
         film.year = parser.get_year(search)
         film.search_tmdb()
@@ -128,31 +189,49 @@ class interactive:
         Returns:
             True if the film passes verification, else False
         """
+
+        # If no matches are found, continually prompt user to find a correct match.
         while len(film.matches) == 0:
             console.colored("%sNo matches found" % MESSAGE_PREFIX, ansi.pink)
             return cls.search_by_name(film)
-        if len(film.matches) > 1:
-            console.white(bold('%sSearch results:' % console.INDENT_PREFIX))
-            choice = cls._choice_input(
-                prompt="Select ", 
-                choices=['%s (%s) [%s]' % (
-                    m.proposed_title, 
-                    m.proposed_year, 
-                    m.tmdb_id) for m in film.matches] + 
-                ['[ Edit search ]', '[ Search by ID ]', '[ Skip ]'],
-                enumeration='number')
-            if choice == len(film.matches):
-                return cls.search_by_name(film)
-            elif choice == len(film.matches) + 1:
-                return cls.lookup_by_id(film)
-            elif choice == len(film.matches) + 2:
-                film.tmdb_id = None
-                film.ignore_reason = 'Skipped'
-                console.dim("Skipped")
-                return False
+
+        console.white(bold('%sSearch results:' % console.INDENT_PREFIX))
+
+        # Generate a list of choices based on search results and save the input
+        # to `choice`.
+        choice = cls._choice_input(
+            prompt="Select ", 
+            choices=['%s (%s) [%s]' % (
+                m.proposed_title, 
+                m.proposed_year, 
+                m.tmdb_id) for m in film.matches] + 
+            ['[ Edit search ]', '[ Search by ID ]', '[ Skip ]'],
+            enumeration='number')
+
+        # If 'Edit search' was selected, try again, then forward
+        # the return value.
+        if choice == len(film.matches):
+            return cls.search_by_name(film)
+
+        # If 'Search by ID' was selected, redirect to ID lookup, then forward
+        # the return value.
+        elif choice == len(film.matches) + 1:
+            return cls.lookup_by_id(film)
+
+        # If skipping, return False
+        elif choice == len(film.matches) + 2:
+            film.tmdb_id = None
+            film.ignore_reason = 'Skipped'
+            console.dim("Skipped")
+            return False
+
+        # If we haven't returned yet, update the film with the selected match 
+        # and mark it as verified.
         film.update_with_match(film.matches[choice])
+        film.tmdb_verified = True
+
         console.search_result(film)
-        return cls.verify_film(film)
+        return True
 
     @classmethod
     def _simple_input(cls, prompt, prefill=''):

commit 025b7d10342ca7c11d2d1df7cf5aa564ddda009e

Fix detection of looked up films (regression from 9a33aa97d33cf7fd8b3c9768d71baffc1c44c14e)

fylm/__init__.py
@@ -75,17 +75,17 @@ def main():
 
                 # If the film should be ignored, print the reason why, and skip.
                 if film.should_ignore is True and config.interactive is False:
-                    console.skip(film, film.ignore_reason)
+                    console.skip(film)
                     continue
 
                 # Print film header to console.
                 console.film_loaded(film)
 
                 # Search TMDb for film details (if enabled).
-                search = film.search_tmdb()
+                film.search_tmdb()
 
-                if search is False and config.interactive is False:
-                    console.skip(film, film.ignore_reason)
+                if film.should_ignore is None and config.interactive is False:
+                    console.skip(film)
                     continue
 
                 # Print the film details to the console.

fylm/fylmlib/film.py
@@ -313,14 +313,11 @@ class Film:
         Calls the tmdb.search() method, passing the current film
         as params. If a result is found, the current film's properties
         are updated with the values from TMDb.
-
-        Returns:
-            True if a suitable match was found, else False
         """
 
         # Only perform lookups if TMDb searching is enabled.
         if config.tmdb.enabled is False:
-            return False
+            return
 
         # Perform the search and save the first 10 results to the matches list. 
         # If ID is not None, search by ID.
@@ -330,11 +327,9 @@ class Film:
         if best_match is not None:
             # If we find a result, update title, tmdb_id, year, and the title_similarity.
             self.update_with_match(best_match)
-            return True
         else:
             # If not, we update the ignore_reason
             self.ignore_reason = 'No results found'
-            return False
 
 
     def update_with_match(self, match):