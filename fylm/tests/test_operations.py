20b80336471552e2ec689dd5243b0d4255f33e2b
Brandon <brandon@behemoth.ca>
2021-03-27 15:55:33 -0700

    Debug tests for Travis

--- fylm/tests/test_operations.py
+++ fylm/tests/test_operations.py
@@ -117,6 +117,9 @@ class TestDirOperations(object):
         assert(len(valid_films) == len(conftest.expected) + 2)
 
         # Assert that the list is sorted alphabetically
+        print(all(valid_films[i].title <= valid_films[i+1].title
+                   for i in range(len(valid_films)-1)))
+        assert(False)
         assert(all(valid_films[i].title <= valid_films[i+1].title
                    for i in range(len(valid_films)-1)))
 
